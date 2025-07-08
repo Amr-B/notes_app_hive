881c6243349125750d7eb229e059601d3f3fffdb
Amr <amr.b4dev@gmail.com>
2025-07-08 16:15:17 +0300

    init hive in main dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromARGB(255, 0, 71, 129);
+const kNotesBox = 'notes box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app_hive/constants.dart';
 import 'package:notes_app_hive/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
