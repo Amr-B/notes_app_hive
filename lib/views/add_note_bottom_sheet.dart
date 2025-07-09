63148a1b92a998e61b1d740a40a10e7591eac6af
Amr <amr.b4dev@gmail.com>
2025-07-09 15:48:55 +0300

    fixed bottom sheet's file & widgets

---
+++ lib/views/add_note_bottom_sheet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_hive/views/widgets/add_note_form.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(child: AddNoteForm()),
+    );
+  }
+}
--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -2,18 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:notes_app_hive/views/widgets/custom_button.dart';
 import 'package:notes_app_hive/views/widgets/custom_text_field.dart';
 
-class AddNoteBottomSheet extends StatelessWidget {
-  const AddNoteBottomSheet({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-      child: SingleChildScrollView(child: AddNoteForm()),
-    );
-  }
-}
-
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({super.key});
 
